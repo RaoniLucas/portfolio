a26de8e67b2cee64b8aab04f69467c0508020612
Raoni Lucas <RaoniLucas123@gmail.com>
2025-12-08 16:11:52 -0300

    testando arquivos

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 console.log("Olá mundo!");
+
+function test() {
+   console.log("Hello World!");
+}
+
+test();
+
+function testHTMLElement() {
+   return `<span>Hello World</span>`;
+}
+
+document.body.appendChild(testHTMLElement());
