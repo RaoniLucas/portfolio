19a0d5df929b8ed3904f1607ea3a1c84cffd87ad
Raoni Lucas <RaoniLucas123@gmail.com>
2025-12-22 16:29:18 -0300

    adicionando layout de menu

--- src/components/menu/menuTemplateElements.js
+++ src/components/menu/menuTemplateElements.js
@@ -1,15 +1,18 @@
 export const menuListenerElements = () => {
    return `
       <header class="header-menu">
-         <span class="switch-container">
-            <div class="switch"></div>
-         </span>
+         <div class="header-menu-container">
+            <span class="switch-container">
+               <div class="switch"></div>
+            </span>
+         </div>
       </header>
-      <nav>
-         <ul>
-            <li></li>
-            <li></li>
-            <li></li>
+      <nav class="nav-menu">
+         <h3 class="subtitle-menu">Menu</h3>
+         <ul class="menu-list">
+            <li class="menu-option">Atualizações</li>
+            <li class="menu-option">Documentos</li>
+            <li class="menu-option">Contatos</li>
          </ul>
       </nav>
       <footer class="footer-menu">
