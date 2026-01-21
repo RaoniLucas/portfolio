84c9b102b247b50fd87831f4930f47df5f5c3b07
Raoni Lucas <RaoniLucas123@gmail.com>
2026-01-21 12:12:55 -0300

    Analisando arquivos

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,4 +1,5 @@
 import { menuListenerElements } from "./menuTemplateElements.js";
+import { windowContact } from "../window/windowTemplate.js";
 import Window from "../window/window.js";
 
 export default class Menu {
@@ -35,35 +36,7 @@ export default class Menu {
 
       document.body.insertAdjacentElement("afterbegin", this.menuContainer);
 
-      // Templates que serão retrabalhados 1
-      const janelaContato = new Window(
-         `
-            <div class="contact-info">
-               <header class="contact-info__header">
-                  <span class="contact-info__title">Contatos</span>
-                  <button class="contact-info__close">Fechar</button>
-               </header>
-               <main class="contact-info__main">
-                  <form class="contact-info__list">
-                     <label for="firstName">Nome</label>
-                     <input id="firstName" type="text" placeholder="Primeiro Nome" name="firstName">
-                     <input id="lastName" type="text" placeholder="Segundo Nome" name="lastName">
-                     
-                     <label for="email">Email</label>
-                     <input id="email" type="email" placeholder="Informe seu email" name="email">
-                     
-                     <label for="subject">Assunto</label>
-                     <input id="subject" type="text" placeholder="Informe o assunto" name="subject">
-                     
-                     <label for="message">Mensagem</label>
-                     <textarea id="message" placeholder="Informe a mensagem" name="message"></textarea>
-                     
-                     <button type="submit">Enviar</button>
-                  </form>
-               </main>
-            </div>
-         `,
-      );
+      const janelaContato = new Window(windowContact);
 
       const menuOption = [...document.querySelectorAll(".menu-option")];
       menuOption.forEach((element, index) => {
