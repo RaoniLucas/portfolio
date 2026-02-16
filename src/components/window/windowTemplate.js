7e56048ebb32dd43a4039bba39ab2e78625af187
Raoni Lucas <RaoniLucas123@gmail.com>
2026-02-16 20:23:54 -0300

    modificando css

--- src/components/window/windowTemplate.js
+++ src/components/window/windowTemplate.js
@@ -2,7 +2,7 @@ export const windowContact = () => {
    return `
       <div class="contact-info">
          <header class="contact-info__header">
-            <span class="contact-info__title">Contatos</span>
+
             <button class="contact-info__close">Fechar</button>
          </header>
          <main class="contact-info__main">
@@ -10,16 +10,16 @@ export const windowContact = () => {
                <label for="firstName">Nome</label>
                <input id="firstName" type="text" placeholder="Primeiro Nome" name="firstName">
                <input id="lastName" type="text" placeholder="Segundo Nome" name="lastName">
-               
+
                <label for="email">Email</label>
                <input id="email" type="email" placeholder="Informe seu email" name="email">
-               
+
                <label for="subject">Assunto</label>
                <input id="subject" type="text" placeholder="Informe o assunto" name="subject">
-               
+
                <label for="message">Mensagem</label>
                <textarea id="message" placeholder="Informe a mensagem" name="message"></textarea>
-               
+
                <button type="submit">Enviar</button>
             </form>
          </main>
